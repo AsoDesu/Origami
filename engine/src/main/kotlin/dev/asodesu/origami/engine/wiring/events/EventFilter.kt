82d58750935aa5c05535af1d213457129dca51e7
Aso <53526710+AsoDesu@users.noreply.github.com>
2024-08-21 20:58:15 +0100

    feat(engine): add EventFilter#all for multi-event filters

--- engine/src/main/kotlin/dev/asodesu/origami/engine/wiring/events/EventFilter.kt
+++ engine/src/main/kotlin/dev/asodesu/origami/engine/wiring/events/EventFilter.kt
@@ -4,4 +4,12 @@ import org.bukkit.event.Event
 
 interface EventFilter {
     fun filter(event: Event): Boolean
+
+    companion object {
+        fun all(vararg filters: EventFilter) = object : EventFilter {
+            override fun filter(event: Event): Boolean {
+                return filters.all { it.filter(event) }
+            }
+        }
+    }
 }
